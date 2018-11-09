0d3a816d60f524579f83ece513944c6d24fd1087
Chaojie Zhang <zyjwdrrr@qq.com>
2018-11-09 10:36:52 +0800

    sync with windows

--- src/messageHandler.py
+++ src/messageHandler.py
@@ -1,17 +1,55 @@
 # -*- coding: utf-8 -*-
 # filename: messageHandler.py
 
+import csv
+
 class User(object):
-    def __init__(self,name,userid,ulevel):
-		self.name = name
-		self.userid = userid
-		self.ulevel = ulevel
+    def __init__(self,name,ulevel):
+	self.name = name
+	self.level = ulevel
     
     def toString(self):
-    	return "your name is " + self.name + "  id: " + self.id + " level: " + self.level
+    	return "your name is " + self.name + " level: " + self.level
 
-    
-userList = []
-userList.append(User("杰","oiDAl1huE6NdA8JAi8ENTtTguHc4",1))
-userList.append(User("VH小叶紫檀","oiDAl1j4jGulYpErdh0umCwOenek",2))
-    
+class UserInfo(object):
+    def __init__(self):
+        self.userDict = {}
+
+    def loadInfo(self):
+        try:
+            userFile = csv.reader(open('user.csv','r'))
+            print userFile
+            next(userFile)
+            for line in userFile:
+                self.userDict[line[1].decode('gb2312')] = User(line[0].decode('gb2312'),line[2].decode('gb2312'))
+            return 1
+        except Exception, Argument:
+            return Argument
+
+    def printInfo(self):
+        print self.userDict['oiDAl1huE6NdA8JAi8ENTtTguHc4'].level
+
+class MSG(object):
+    def __init__(self,reply,price):
+        self.reply = reply
+        self.price = price
+
+class MsgInfo(object):
+    def __init__(self):
+        self.msgDict = {}
+
+    def loadMsg(self):
+        try:
+            msgFile = csv.reader(open('msg1.csv','r'))
+            print msgFile
+            next(msgFile)
+            for line in msgFile:
+                levelList = []
+                for l in line[2:12]:
+                    levelList.append(int(l))
+                self.msgDict[line[0].decode('gb2312')] = MSG(line[1].decode('gb2312'),levelList)
+            return 1    
+        except Exception, Argument:
+            return Argument
+
+
